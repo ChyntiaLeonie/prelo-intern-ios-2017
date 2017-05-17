2266535bb93381be94d3fdb4babf0ccfe937be41
Chyntia Leonie <chyn_leo038@yahoo.co.id>
2017-05-17 17:58:45 +0700

    initial commit

---
+++ prelo-intern-ios-2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prelo-intern-ios-2017
+//
+//  Created by Chyntia Leonie Andreas on 5/17/17.
+//  Copyright © 2017 Chyntia Leonie Andreas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
