65027196adb3d074ebe4f786b7272bfef38d2bf7
Chyntia Leonie <chyn_leo038@yahoo.co.id>
2017-05-18 11:42:51 +0700

    update fix layout

--- prelo-intern-ios-2017/ViewController.swift
+++ prelo-intern-ios-2017/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let image = UIImage(named: "prelo.png")
+        let imageView = UIImageView(image: image)
+        imageView.image = imageView.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        imageView.tintColor = UIColor.whiteColor()
+        self.navigationItem.titleView = imageView
+        
+        self.navigationController!.navigationBar.barTintColor = UIColor(red: 0, green: 0.655, blue: 0.616, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
