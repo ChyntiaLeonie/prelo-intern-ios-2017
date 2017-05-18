5b9faf1bfd24400ad2e7507afb65413491863e95
Chyntia Leonie <chyn_leo038@yahoo.co.id>
2017-05-18 15:53:02 +0700

    Access API get Token

--- prelo-intern-ios-2017/ViewController.swift
+++ prelo-intern-ios-2017/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var token : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,13 +20,50 @@ class ViewController: UIViewController {
         imageView.image = imageView.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
         imageView.tintColor = UIColor.whiteColor()
         self.navigationItem.titleView = imageView
-        
         self.navigationController!.navigationBar.barTintColor = UIColor(red: 0, green: 0.655, blue: 0.616, alpha: 1)
+        accessAPI()
+
+        while(self.token=="")
+        {
+            
+        }
+        print("Token",self.token)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func accessAPI(){
+        let username_or_email = "frontend"
+        let password = "intern"
+        
+        let req = NSMutableURLRequest(URL:NSURL(string:"https://dev.prelo.id/api/auth/login")!)
+        
+        let bodyData = "username_or_email=" + username_or_email + "&password=" + password
+        
+        req.HTTPMethod = "POST"
+        req.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let dataTask = session.dataTaskWithRequest(req,completionHandler: {(data,response,error) in
+            
+            if let convertedJsonIntoDict = try? NSJSONSerialization.JSONObjectWithData(data!, options: []){
+                
+                // Print out dictionary
+                //print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                let isiData = convertedJsonIntoDict["_data"]!
+                self.token = isiData!["token"] as! String
+            } else {
+                print("masuknya kesini")
+            }
+            }
+        )
+        dataTask.resume()
     }
 
 
