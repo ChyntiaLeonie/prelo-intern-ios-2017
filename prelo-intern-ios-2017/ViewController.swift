6e9e223cde52f0902a77b46de8aea311f02a52cc
Chyntia Leonie <chyn_leo038@yahoo.co.id>
2017-05-18 20:41:42 +0700

    Final LoveList

--- prelo-intern-ios-2017/ViewController.swift
+++ prelo-intern-ios-2017/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
     var token : String = ""
-    
-    let cellReuseIdentifier = "cellLoveList"
+    var allProduct:NSMutableArray = NSMutableArray()
+    var unDone : Bool = true
     
     @IBOutlet weak var tableView: UITableView!
+    let cellReuseIdentifier = "cellLoveList"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +33,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             
         }
         print("Token",self.token)
+        
+//        print("Token",self.token)
+        accessProductAPI()
+        while(unDone){
+            
+        }
         tableView.delegate = self
     }
 
@@ -69,20 +76,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         )
         dataTask.resume()
     }
+    
+    func accessProductAPI(){
+        let req = NSMutableURLRequest(URL:NSURL(string:"https://dev.prelo.id/api/me/lovelist")!)
+        
+        req.HTTPMethod = "GET"
+        req.setValue("Token "+token, forHTTPHeaderField: "Authorization")
+        
+        let session = NSURLSession.sharedSession()
+        
+        let dataTask = session.dataTaskWithRequest(req,completionHandler: {(data,response,error) in
+            
+            if let convertedJsonIntoDict = try? NSJSONSerialization.JSONObjectWithData(data!, options: []){
+                
+                // Print out dictionary
+                //print("convertedJsonIntoDict \(convertedJsonIntoDict)")
+                
+                var pLoveList : ProductLoveList = ProductLoveList()
+                
+                if let array = convertedJsonIntoDict["_data"] as? [AnyObject] {
+                    for object in array {
+                        pLoveList = ProductLoveList()
+                        
+                        if let prodImage = object["display_picts"] as? [AnyObject] {
+                            pLoveList.imageProduct = prodImage[0] as! String
+                        }
+                        
+                        pLoveList.titleProduct = object["name"]! as! String
+                        pLoveList.priceProduct = object["price"]! as! Int
+
+                        pLoveList.commentProduct = object["num_comment"]! as! Int
+
+                        pLoveList.likeProduct = object["num_lovelist"]! as! Int
+
+                        self.allProduct.addObject(pLoveList)
+                    }
+                }
+                self.unDone = false;
+                
+            } else {
+                print("masuknya kesini")
+            }
+            }
+        )
+        dataTask.resume()
+    }
+    
 
     // number of rows in table view
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return self.allProduct.count
     }
     
     // create a cell for each table view row
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell:LoveListCell = self.tableView.dequeueReusableCellWithIdentifier(cellReuseIdentifier) as! LoveListCell
-        cell.imageView?.image = UIImage(named: "prelo.png")
-        cell.titleProduct.text = "cobainlalala"
-        cell.priceProduct.text = "Rp 0"
-        cell.commentProduct.text = "0"
-        cell.likeProduct.text = "1"
+        
+        let product = allProduct[indexPath.row] as! ProductLoveList
+        
+        if let url = NSURL(string: product.imageProduct) {
+            if let data = NSData(contentsOfURL: url) {
+                cell.imageProduct.image = UIImage(data: data)
+            }        
+        }
+        
+        cell.titleProduct.text = product.titleProduct
+        cell.priceProduct.text = "Rp "+String(product.priceProduct)
+        cell.commentProduct.text = String(product.commentProduct)
+        cell.likeProduct.text = String(product.likeProduct)
         return cell;
     }
     
