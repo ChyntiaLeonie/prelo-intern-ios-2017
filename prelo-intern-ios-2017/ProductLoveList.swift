e187ae52c3596b95161bd1ed4151761248c67f0c
Chyntia Leonie <chyn_leo038@yahoo.co.id>
2017-05-18 20:19:25 +0700

    update access product API

---
+++ prelo-intern-ios-2017/ProductLoveList.swift
@@ -0,0 +1,19 @@
+//
+//  ProductLoveList.swift
+//  prelo-intern-ios-2017
+//
+//  Created by Chyntia Leonie Andreas on 5/18/17.
+//  Copyright © 2017 Chyntia Leonie Andreas. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class ProductLoveList: NSObject {
+    
+    var imageProduct: String = String()
+    var titleProduct: String = String()
+    var priceProduct: Int = Int()
+    var commentProduct: Int = Int()
+    var likeProduct: Int = Int()
+    
+}
